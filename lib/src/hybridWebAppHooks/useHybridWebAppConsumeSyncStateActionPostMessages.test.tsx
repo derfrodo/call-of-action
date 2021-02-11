7bf69f5b89ed311e017d971940eeeb5787e056d3
Stefan Pauka <paukastefan@gmail.com>
2021-02-11 16:50:33 +0100

    add first tests yay

---
+++ lib/src/hybridWebAppHooks/useHybridWebAppConsumeSyncStateActionPostMessages.test.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useHybridWebAppConsumeSyncStateActionPostMessages } from "./useHybridWebAppConsumeSyncStateActionPostMessages";
+import { renderHook } from "@testing-library/react-hooks";
+import { ActionTypeguard, SyncStateAction } from "../types";
+
+const makeWrapper = (): React.FC => ({ children }) => <>{children}</>;
+
+type TestActionClass = { isBubbled?: boolean };
+describe("Given useHybridWebAppConsumeSyncStateActionPostMessages", () => {
+    it("when called, it returns void", async () => {
+        const onMessage = jest.fn<
+            void | Promise<void>,
+            [SyncStateAction<TestActionClass>]
+        >();
+
+        const isActionTypeguard = (jest.fn() as unknown) as jest.MockedFunction<
+            ActionTypeguard<TestActionClass>
+        >;
+
+        const { result } = renderHook(
+            () =>
+                useHybridWebAppConsumeSyncStateActionPostMessages(
+                    onMessage,
+                    isActionTypeguard
+                ),
+            {
+                wrapper: makeWrapper(),
+            }
+        );
+        expect(result.current).toBeUndefined();
+    });
+});

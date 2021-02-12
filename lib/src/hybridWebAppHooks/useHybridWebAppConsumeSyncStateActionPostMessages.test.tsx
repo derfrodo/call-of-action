19a1abcaa104b579132a830ecbde0a6130b7b96f
Stefan Pauka <paukastefan@gmail.com>
2021-02-12 09:12:03 +0100

    add tests

--- lib/src/hybridWebAppHooks/useHybridWebAppConsumeSyncStateActionPostMessages.test.tsx
+++ lib/src/hybridWebAppHooks/useHybridWebAppConsumeSyncStateActionPostMessages.test.tsx
@@ -6,7 +6,101 @@ import { ActionTypeguard, SyncStateAction } from "../types";
 const makeWrapper = (): React.FC => ({ children }) => <div>{children}</div>;
 
 type TestActionClass = { isBubbled?: boolean };
+
+const fakePostMessageEvent = () => {};
+const windowEventListenerMock = jest.fn();
+const documentEventListenerMock = jest.fn();
+
 describe("Given useHybridWebAppConsumeSyncStateActionPostMessages", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+        window.addEventListener = windowEventListenerMock;
+        (document as any).addEventListener = documentEventListenerMock;
+    });
+
+    it("when called, it hooks on window message event for non-iOS Webview", async () => {
+        const onMessage = jest.fn<
+            void | Promise<void>,
+            [SyncStateAction<TestActionClass>]
+        >();
+
+        const isActionTypeguard = (jest.fn() as unknown) as jest.MockedFunction<
+            ActionTypeguard<TestActionClass>
+        >;
+
+        const { result } = renderHook(
+            () =>
+                useHybridWebAppConsumeSyncStateActionPostMessages(
+                    onMessage,
+                    isActionTypeguard
+                ),
+            {
+                wrapper: makeWrapper(),
+            }
+        );
+        expect(windowEventListenerMock).toBeCalledWith(
+            "message",
+            expect.anything(),
+            undefined
+        );
+    });
+
+    it("when called, it hooks on document message event for iOS webViews", async () => {
+        const onMessage = jest.fn<
+            void | Promise<void>,
+            [SyncStateAction<TestActionClass>]
+        >();
+
+        const isActionTypeguard = (jest.fn() as unknown) as jest.MockedFunction<
+            ActionTypeguard<TestActionClass>
+        >;
+
+        const { result } = renderHook(
+            () =>
+                useHybridWebAppConsumeSyncStateActionPostMessages(
+                    onMessage,
+                    isActionTypeguard
+                ),
+            {
+                wrapper: makeWrapper(),
+            }
+        );
+        expect(documentEventListenerMock).toBeCalledWith(
+            "message",
+            expect.anything()
+        );
+    });
+
+    it("when called and failed on binding window message event, then hook will still listen on document events", async () => {
+        const onMessage = jest.fn<
+            void | Promise<void>,
+            [SyncStateAction<TestActionClass>]
+        >();
+
+        const isActionTypeguard = (jest.fn() as unknown) as jest.MockedFunction<
+            ActionTypeguard<TestActionClass>
+        >;
+        windowEventListenerMock.mockImplementation((a1) => {
+            if (a1 === "message") {
+                throw new Error("Testerror");
+            }
+        });
+        const { result } = renderHook(
+            () =>
+                useHybridWebAppConsumeSyncStateActionPostMessages(
+                    onMessage,
+                    isActionTypeguard
+                ),
+            {
+                wrapper: makeWrapper(),
+            }
+        );
+        expect(windowEventListenerMock).toBeCalled();
+        expect(documentEventListenerMock).toBeCalledWith(
+            "message",
+            expect.anything()
+        );
+    });
     it("when called, it returns void", async () => {
         const onMessage = jest.fn<
             void | Promise<void>,
